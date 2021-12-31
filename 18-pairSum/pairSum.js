2d7a80886687db4256702ded60f48dac3fc9647b
Marcin Tomala <marcin.p.tomala@gmail.com>
2021-12-30 21:12:55 -0500

    done 18-19

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,16 @@
 function pairSum(nums, target) {
-  // Insert code here;
+  if (nums.length < 2) {
+    throw "Length of 'nums' is less than 2, this isn't going to work."
+  }
+  const numSet = new Set();
+  for (let i = 0; i < nums.length; i++) {
+    if (numSet.has(target - nums[i])) {
+      return true;
+    } else {
+      numSet.add(nums[i]);
+    }
+  }
+  return false;
 }
 
 // Do not edit this line;
--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -2,12 +2,37 @@ class MySolution {
   constructor() {
     this.flag = false; // this is optional to use;
   }
-
-  binarySearch(nums, target) {
-    // Insert code here;
+  binarySearch(nums, target, start = 0, end = nums.length-1) {
+    let mid = Math.floor((start + end)/2);
+    if (start == end) {
+      return false;
+    }
+    if (target == nums[mid]) {
+      return true;
+    } else if (target > nums[mid]) {
+      return this.binarySearch(nums, target, mid+1, end); 
+    } else {
+      return this.binarySearch(nums, target, start, mid);
+    }
   }
+  // Or, using only parameters (nums, target):
+  //
+  // binarySearch(nums, target) {
+  //   let mid = Math.floor(nums.length/2);
+  //   if (nums.length == 0) {
+  //     return false;
+  //   }
+  //   if (target == nums[mid]) {
+  //     return true;
+  //   } else if (target > nums[mid]) {
+  //     const aux = nums.slice(mid+1, nums.length);
+  //     return this.binarySearch(aux, target)
+  //   } else {
+  //     const aux = nums.slice(0, mid);
+  //     return this.binarySearch(aux, target)
+  //   }
+  // }
 }
-
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
